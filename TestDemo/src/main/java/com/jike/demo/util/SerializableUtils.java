f68f4932a5c38a4287ee53e992a035805607ff38
qukun <qu.kun@yanwei365.com>
2020-09-16 17:55:16 +0800

    yw redis

--- TestDemo/src/main/java/com/jike/demo/util/SerializableUtils.java
+++ TestDemo/src/main/java/com/jike/demo/util/SerializableUtils.java
@@ -1,5 +1,8 @@
 package com.jike.demo.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
@@ -11,15 +14,19 @@ import java.io.ObjectOutputStream;
  * @description 序列化&反序列化工具类
  */
 public class SerializableUtils {
+    private static final Logger logger = LoggerFactory.getLogger(SerializableUtils.class);
+
     /**
      * 序列化
-     * @param object 序列化对象
+     *
+     * @param object     序列化对象
      * @param targetPath 序列化写入磁盘路径及文件名
      */
     public static void serializable(Object object, String targetPath) {
         try (FileOutputStream fos = new FileOutputStream(targetPath);
              ObjectOutputStream oos = new ObjectOutputStream(fos)) {
             oos.writeObject(object);
+            logger.info("-------对象序列化成功:{}-------", targetPath);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -27,13 +34,14 @@ public class SerializableUtils {
 
     /**
      * 反序列化
+     *
      * @param targetPath 反序列化文件路径及文件名
      */
     public static void deSerializable(String targetPath) {
-        try(FileInputStream sfis = new FileInputStream(targetPath);
-            ObjectInputStream sois = new ObjectInputStream(sfis)) {
+        try (FileInputStream sfis = new FileInputStream(targetPath);
+             ObjectInputStream sois = new ObjectInputStream(sfis)) {
             Object object = sois.readObject();
-            System.out.println(object);
+            logger.info("-------对象反序列化成功:{}-------", object);
         } catch (Exception e) {
             e.printStackTrace();
         }
