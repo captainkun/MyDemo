4585792b6ed3d12f6c19ab3f725edd0321a35237
qukun <qu.kun@yanwei365.com>
2021-01-27 19:24:32 +0800

    redis log

--- TestDemo/src/main/java/com/jike/demo/config/RedisConfig.java
+++ TestDemo/src/main/java/com/jike/demo/config/RedisConfig.java
@@ -11,6 +11,7 @@ import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import redis.clients.jedis.JedisPoolConfig;
 
@@ -123,8 +124,17 @@ public class RedisConfig {
 	}
 
 	private void initDomainRedisTemplate(RedisTemplate<String, Object> redisTemplate, RedisConnectionFactory factory) {
-		// 如果不配置Serializer，那么存储的时候缺省使用String，如果用User类型存储，那么会提示错误User can't cast to
-		// String！
+		// 如果不配置Serializer，那么存储的时候缺省使用String，如果用User类型存储，那么会提示错误User can't cast to String！
+		// 默认情况下，RedisTemplate 使用 JdkSerializationRedisSerializer，也就是 JDK 序列化，容易产生 Redis 中保存了乱码的错觉。
+		// 通常考虑到易读性，可以设置 Key 的序列化器为 StringRedisSerializer。
+		// 但直接使用 RedisSerializer.string()，相当于使用了 UTF_8 编码的 StringRedisSerializer，需要注意字符集问题。
+		// 如果希望 Value 也是使用 JSON 序列化的话，可以把 Value 序列化器设置为 Jackson2JsonRedisSerializer。
+		// 默认情况下，不会把类型信息保存在 Value 中，
+		// 即使我们定义 RedisTemplate 的 Value 泛型为实际类型，查询出的 Value 也只能是 LinkedHashMap 类型。
+		// 如果希望直接获取真实的数据类型，
+		// 可以启用 Jackson ObjectMapper 的 activateDefaultTyping 方法，把类型信息一起序列化保存在 Value 中。
+		// 如果希望 Value 以 JSON 保存并带上类型信息，
+		// 更简单的方式是，直接使用 RedisSerializer.json() 快捷方法来获取序列化器。
 		redisTemplate.setKeySerializer(new StringRedisSerializer());
 		redisTemplate.setHashKeySerializer(new StringRedisSerializer());
 		redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
